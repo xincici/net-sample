fbbc992a9a457ca2bdac7195785d24c523d31569
xincici <llxy8687@foxmail.com>
2026-08-05 11:08:35 +0800

    fix cjk character error and add chat-client

---
+++ client.mjs
@@ -0,0 +1,5 @@
+import ChatClient from './lib/chat-client.mjs';
+
+var client = new ChatClient(9999, '0');
+
+client.connect();
--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import Server from './lib/server.mjs';
 
-var app = new Server(9999, '127.0.0.1');
+var app = new Server(9999, '0');
 
 app.start();
